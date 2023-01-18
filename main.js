544c3b319fda9c25c36d06783eecb15aa7579538
freshc0w <121275444+freshc0w@users.noreply.github.com>
2023-01-18 05:50:07 +0000

    Add draw grid function based on book objs

--- main.js
+++ main.js
@@ -1,11 +1,34 @@
 const addBtn = document.querySelector('.addBtn');
 const content = document.querySelector('.content')
+let myLibrary = [];
 
-const addCard = () => {
-    let card = document.createElement('div');
-    card.classList.add('card');
-    card.textContent = 'Something5';
-    content.insertBefore(card, content.firstChild);
+// function addCard() {
+//     let card = document.createElement('div');
+//     card.classList.add('card');
+    
+//     // We can appendChild the contents of the book info here.
+//     card.textContent = 'Something5';
+//     content.insertBefore(card, content.firstChild);
+// }
+
+function drawGrid() {
+    for(let i = 0; i < myLibrary.length; i++) {
+        let card = document.createElement('div')
+        card.classList.add('card');
+        card.textContent = myLibrary[i].name
+        content.appendChild(card);
+
+    }
 }
 
-addBtn.addEventListener('click', addCard)
+
+function Book(name) {
+    this.name = name
+}
+
+function addBookToLibrary(name) {
+    let book = new Book(name)
+    myLibrary.unshift(book)
+    console.log(myLibrary)
+}
+
